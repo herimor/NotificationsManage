b795acfee1cfbcf1f6ec94b70084c176f67655f7
Никита Торгашов <herimor@MacBook-Air-Nikita.local>
2019-05-02 18:12:38 +0300

    Add missed files

---
+++ NotificationDB/NotificationDB/StoredProcedures.sql
@@ -0,0 +1,165 @@
+﻿
+-- SelectActualEntries
+
+SELECT
+	sc.ID,
+	sc.PhoneNumber,
+	sc.AppointmentDate,
+	sc.Doctor_id,
+	d.FirstName,
+	d.SecondName,
+	d.Patronymic,
+	d.Specialization_id,
+	sc.SendingStatus
+FROM Schedule sc
+	LEFT JOIN Doctors d ON sc.Doctor_id = d.ID
+WHERE sc.AppointmentDate < @AppointmentDate
+	AND sc.SendingStatus = 1
+	AND sc.TransactionId IS NULL
+
+-- AddSchedule
+
+INSERT INTO Schedule(
+	Doctor_id, 
+	PhoneNumber, 
+	AppointmentDate, 
+	SendingStatus)
+VALUES(
+	@DoctorId,
+	@PhoneNumber,
+	@AppointmentDate,
+	@SendingStatus)
+
+-- DeleteSchedule
+
+DELETE FROM Schedule 
+WHERE ID = @ScheduleID
+
+-- UpdateSchedule
+
+UPDATE Schedule
+SET 
+	Doctor_id = @DoctorId,
+	PhoneNumber = @PhoneNumber,
+	AppointmentDate = @AppointmentDate,
+	SendingStatus = @SendingStatus,
+	TransactionId = @TransactionId
+WHERE ID = @ScheduleID
+
+-- PartialScheduleUpdate
+
+UPDATE Schedule
+SET 
+	Doctor_id = @DoctorId,
+	PhoneNumber = @PhoneNumber,
+	AppointmentDate = @AppointmentDate
+WHERE ID = @ScheduleID
+
+-- AddDoctor
+
+INSERT INTO Doctors(
+	FirstName, 
+	SecondName, 
+	Patronymic, 
+	Specialization_id)
+VALUES(
+	@FirstName,
+	@SecondName,
+	@Patronymic,
+	@SpecializationID)
+
+-- DeleteDoctor
+
+DELETE FROM Doctors WHERE ID = @DoctorID
+
+-- UpdateDoctor
+
+UPDATE Doctors
+SET 
+	FirstName = @FirstName,
+	SecondName = @SecondName,
+	Patronymic = @Patronymic,
+	Specialization_id = @SpecializationID
+WHERE ID = @DoctorID
+
+-- GetAllDoctors
+
+SELECT 
+	ID, 
+	FirstName, 
+	SecondName, 
+	Patronymic, 
+	Specialization_id 
+FROM DOCTORS
+
+-- GetAllSchedule
+
+SELECT
+	sc.ID,
+	sc.PhoneNumber,
+	sc.AppointmentDate,
+	sc.Doctor_id,
+	d.FirstName,
+	d.SecondName,
+	d.Patronymic,
+	d.Specialization_id,
+	sc.SendingStatus,
+	sc.TransactionId
+FROM Schedule sc
+	LEFT JOIN Doctors d ON sc.Doctor_id = d.ID
+
+-- GetScheduleByDoctorId
+
+SELECT
+	sc.ID,
+	sc.PhoneNumber,
+	sc.AppointmentDate,
+	sc.Doctor_id,
+	d.FirstName,
+	d.SecondName,
+	d.Patronymic,
+	d.Specialization_id,
+	sc.SendingStatus,
+	sc.TransactionId
+FROM Schedule sc
+	LEFT JOIN Doctors d ON sc.Doctor_id = d.ID
+WHERE d.ID = @DoctorID
+
+-- AddUser
+
+INSERT INTO Users(
+	Login, 
+	Password) 
+VALUES(
+	@Login, 
+	@Password)
+
+-- GetUserByLogin
+
+SELECT 
+	Login, 
+	Password 
+FROM Users 
+WHERE Login = @Login
+
+-- GetTransactedSchedule
+
+SELECT
+	sc.ID,
+	sc.PhoneNumber,
+	sc.AppointmentDate,
+	sc.Doctor_id,
+	d.FirstName,
+	d.SecondName,
+	d.Patronymic,
+	d.Specialization_id,
+	sc.SendingStatus,
+	sc.TransactionId
+FROM Schedule sc
+	LEFT JOIN Doctors d ON sc.Doctor_id = d.ID
+WHERE sc.TransactionId IS NOT NULL
+
+-- DeleteObsoleteSchedule
+
+DELETE FROM Schedule 
+WHERE AppointmentDate < @AppointmentDate
---
+++ NotificationDB/NotificationDB/Tables.sql
@@ -0,0 +1,29 @@
+﻿
+CREATE TABLE Doctors(
+ID integer primary key autoincrement,
+FirstName varchar(50),
+SecondName varchar(50),
+Patronymic varchar(50),
+Specialization_id integer
+);
+
+CREATE TABLE Schedule(
+ID integer primary key autoincrement,
+Doctor_id integer,
+PhoneNumber varchar,
+AppointmentDate datetime,
+SendingStatus integer,
+TransactionId integer,
+FOREIGN KEY (Doctor_id) REFERENCES Doctors(ID)
+);
+
+CREATE TABLE Procedures(
+ID integer primary key autoincrement,
+ProcedureText varchar(255)
+);
+
+CREATE TABLE Users(
+ID integer primary key autoincrement,
+Login varchar(255),
+Password varchar(255)
+);
